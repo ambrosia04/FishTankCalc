5214fa8d4fe9cb49d9ff147d844c67f9820a1811
Marta <martagranado1011@uma.es>
2026-04-04 15:39:02 +0200

    Delete scraper.py

--- scraper.py
+++
@@ -1,9 +0,0 @@
-import requests
-from bs4 import BeautifulSoup
-
-url = "https://www.fishipedia.es/pez/betta-splendens"
-html = requests.get(url).text
-
-soup = BeautifulSoup(html, "html.parser")
-
-print(soup.text)
